ff80ae8c6a4501807ef247bdc2ebf47f667251a3
ilias <mail@ilias.xyz>
2023-12-18 17:15:32 +0100

    Initialize our nixos module

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Base nixos configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosModules.default = { pkgs, ... }: {
+      environment.systemPackages = [ pkgs.git ];
+    };
+  };
+}
