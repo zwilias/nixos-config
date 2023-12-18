27f6d0de3d6bd2501668b0e371ae82fad513a351
ilias <mail@ilias.xyz>
2023-12-18 17:52:42 +0100

    Try to add old fwupd in there

--- flake.nix
+++ flake.nix
@@ -3,6 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+    # To successfully flash the firmware of the onboard fingerprint reader on
+    # Ryzen 7040 framework laptops, we need version 1.9.7 of fwupd. See
+    # https://knowledgebase.frame.work/en_us/updating-fingerprint-reader-firmware-on-linux-for-13th-gen-and-amd-ryzen-7040-series-laptops-HJrvxv_za
+    # for more info.
+    nixpkgs-old-fwupd.url =
+      "github:NixOS/nixpkgs/a845c1b2d62614f80de711d7cecbd0688c53429e";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       # The `follows` keyword in inputs is used for inheritance.
@@ -17,13 +25,29 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosModules.default = { pkgs, ... }: {
-      environment.systemPackages = [
-        # General tooling
-        pkgs.git
-        pkgs.nixfmt
-      ];
-    };
+    nixosModules.default = { pkgs, ... }:
+      let
+        fwupd-old =
+          (import inputs.nixpkgs-old-fwupd { system = pkgs.system; }).fwupd;
+      in {
+        # Default settings
+        boot.loader.systemd-boot.enable = true;
+        boot.loader.efi.canTouchEfiVariables = true;
+        networking.networkmanager.enable = true;
+
+        # Enable firmware upgrade and fingerprint services
+        services.fwupd.enable = true;
+        services.fwupd.package = fwupd-old;
+
+        services.fprintd.enable = true;
+
+        environment.systemPackages = [
+          # General tooling
+          pkgs.git
+          pkgs.nixfmt
+          pkgs.vim # I mean, it's better than nano
+        ];
+      };
 
     # Module for 13-inch Ryzen 7040 framework hardware quirks
     nixosModules.framework-13-7040-amd =
